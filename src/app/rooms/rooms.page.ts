41f190dfd6d49730ba9cb16fab810d030ecf2a4c
ymurata <m.yuma0531@gmail.com>
2019-02-20 17:48:06 +0900

    add menu button and action sheet

--- src/app/rooms/rooms.page.ts
+++ src/app/rooms/rooms.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import {
   FirebaseBackendService,
   RoomStats,
@@ -13,7 +14,9 @@ import { Observable } from 'rxjs';
 export class RoomsPage implements OnInit {
   activeRooms$: Observable<RoomStats[]>;
 
-  constructor(private readonly firebaseBackend: FirebaseBackendService) {}
+  constructor(private readonly firebaseBackend: FirebaseBackendService,
+              private readonly actionSheetController: ActionSheetController) {
+  }
 
   ngOnInit() {
     this.activeRooms$ = this.firebaseBackend.activeRooms$;
@@ -22,4 +25,32 @@ export class RoomsPage implements OnInit {
   trackRoom(index: number, room: RoomStats) {
     return room.key;
   }
+
+  async shouldShowMenu() {
+    // TODO: add action handler
+    const actionSheet = await this.actionSheetController.create({
+      header: 'メニュー',
+      buttons: [
+        {
+          text: '部屋を作成する',
+          icon: 'add',
+          role: 'create room',
+          handler: () => {},
+        },
+        {
+          text: 'ニックネームをつける',
+          icon: 'person',
+          role: 'edit name',
+          handler: () => {},
+        },
+        {
+          text: 'キャンセル',
+          icon: 'close',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
 }
